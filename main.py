8e617a740a97ad30e0db044e64db942a82ce7676
Ignacio Gómez-Centurión Golmayo <igomezcenturiong@student.universidadunie.com>
2025-04-06 16:45:29 +0200

    editar

--- main.py
+++ main.py
@@ -4,11 +4,7 @@ from Game import Game
 if __name__ == "__main__":
     game = Game()
     game.start()
-
-    while game.is_running:
-        game.update()
-        # Add a condition to break the loop and end the game
-        user_input = input("Type 'exit' to end the game: ")
-        if user_input.lower() == 'exit':
-            game.end_game()
-        break
+    game.update()
+    game.end_game()
+    print(game)
+    game.reset_game()
